c290a49fe79cd82ce6eeeb9e82d9b5dd19bb234c
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2022-07-27 11:17:52 +0100

    Implements compontents props to ExpenseItems

--- new-react-app/src/App.js
+++ new-react-app/src/App.js
@@ -1,10 +1,38 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      amount: 32.50,
+      title: 'Car Insurance',
+      date: new Date(2022, 7, 27)
+    },
+    {
+      id: 2,
+      amount: 50,
+      title: 'Gym',
+      date: new Date(2022, 7, 27)
+    },
+    {
+      id: 3,
+      amount: 12,
+      title: 'Mobile',
+      date: new Date(2022, 7, 27)
+    }
+  ]
   return (
     <div>
       <h2>Let's get started! Great...</h2>
-      <ExpenseItem />
+      {expenses.map(expense => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            amount={expense.amount}
+            title={expense.title}
+            date={expense.date} 
+          />)
+      })}
     </div>
   );
 }
--- new-react-app/src/components/ExpenseItem.js
+++ new-react-app/src/components/ExpenseItem.js
@@ -1,39 +1,16 @@
-const date = new Date()
-const day = date.getDate();
-const month = date.getMonth();
-const year = date.getFullYear();
+import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
-const array = [
-  {
-    first: "Matt",
-    second: "Berenyi",
-    age: "31"
-  },
-  {
-    first: "Emma",
-    second: "Foulds",
-    age: "32"
-  },
-  {
-    first: "Paul",
-    second: "Foulds",
-    age: "37"
-  },
-]
-
-function ExpenseItem() {
-    return (
-        <div>
-          <h1>Todays date is: <span>{day + '-' + month + '-' + year}</span></h1>
-          {array.map(object => {
-            return <div>
-              <p>Firstname: {object.first}</p>
-              <p>Surname:{object.second}</p>
-              <p>Age: {object.age}</p>
-            </div>
-          })}
-        </div>
-    )
+function ExpenseItem (props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">£{props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
